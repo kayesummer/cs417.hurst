ad64f98a8ed23c0374058240828ff7b57e53bf88
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-08-23 16:41:17 -0500

    getting started

---
+++ apps/asg1/asg1.cpp
@@ -0,0 +1,19 @@
+//
+// File:    asg1.cpp
+// Author: Kaylee Hurst  
+// Purpose:
+// Given a directed graph, this program will check to see if a user-specified path from that graph contains a cycle
+//
+
+#include <iostream> 
+#include "adjacentcylist.h"
+#include "adjacentcymatrix.h"
+
+using namespace std;
+
+int main ()
+{
+
+
+    return 0;
+}
