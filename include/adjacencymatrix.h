07b4c76534902de9deba8464c71c3db971bb8269
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-08-31 14:14:07 -0500

    example

--- include/adjacencymatrix.h
+++ include/adjacencymatrix.h
@@ -14,11 +14,11 @@ class AdjacencyMatrix : public Graph<ValueType>
 
     public: 
     // Assume that the class is templated by the type "ValueType"
-    bool adjacent(ValueType x, ValueType y); // is there a node from x to y
-    vector<ValueType> neighbors(ValueType x); // Return a vector of neighbor nodes
-    void addEdge(ValueType source, ValueType dest); //add an edge from source, to dest
+    virtual bool adjacent(ValueType x, ValueType y); // is there a node from x to y
+    virtual vector<ValueType> neighbors(ValueType x); // Return a vector of neighbor nodes
+    virtual void addEdge(ValueType source, ValueType dest); //add an edge from source, to dest
                                                     // add dest if it isn't already in graph
-    void addNode(ValueType x); //Add x to graph
-    void deleteEdge(ValueType source, ValueType dest);
-    void deleteNode(ValueType node);
+    virtual void addNode(ValueType x); //Add x to graph
+    virtual void deleteEdge(ValueType source, ValueType dest);
+    virtual void deleteNode(ValueType node);
 };
--- include/graph.h
+++ include/graph.h
@@ -2,21 +2,22 @@
 // File:    graph.h
 // Author: Kaylee Hurst  
 // Purpose:
-// Implement a virtual base class for a graph to be inherited by adjacencylist.h and adjacencymatrix.h
+// Implement a pure virtual base class for a graph to be inherited by adjacencylist.h and adjacencymatrix.h
 //
+
+#include <vector>
 
 template <typename ValueType> 
 class Graph
 {
-    private: 
-
     public:
-        // Assume that the class is templated by the type "ValueType"
-        virtual bool adjacent(ValueType x, ValueType y) = 0; // is there a node from x to y
-        virtual vector<ValueType> neighbors(ValueType x) = 0; // Return a vector of neighbor nodes
-        virtual void addEdge(ValueType source, ValueType dest) = 0; //add an edge from source, to dest
-                                                        // add dest if it isn't already in graph
-        virtual void addNode(ValueType x) = 0;  //Add x to graph
+
+        //add constructor/destructor hehe
+
+        virtual bool adjacent(ValueType x, ValueType y) = 0; 
+        virtual vector<ValueType> neighbors(ValueType x) = 0; 
+        virtual void addEdge(ValueType source, ValueType dest) = 0; 
+        virtual void addNode(ValueType x) = 0; 
         virtual void deleteEdge(ValueType source, ValueType dest) = 0;
         virtual void deleteNode(ValueType node) = 0;
 
