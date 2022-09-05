aa02abe34e5dca905714d396645ef9ec9d2f35ba
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-09-05 15:50:47 -0500

    tree.h ready

--- include/adjacencylist.h
+++ include/adjacencylist.h
@@ -6,8 +6,8 @@
 //
 
 #pragma once 
-#include <vector>   //to use the vector ADT
-#include <list>     //to use the list ADT 
+#include <vector>   
+#include <list>     
 #include "graph.h"      //to inherit from the graph class
 
 template <typename ValueType>
@@ -17,11 +17,28 @@ class AdjacencyList : public Graph<ValueType>
 
     public: 
     // Assume that the class is templated by the type "ValueType"
-    virtual bool adjacent(ValueType x, ValueType y); // is there a node from x to y
-    virtual vector<ValueType> neighbors(ValueType x); // Return a vector of neighbor nodes
-    virtual void addEdge(ValueType source, ValueType dest); //add an edge from source, to dest
-                                                    // add dest if it isn't already in graph
-    virtual void addNode(ValueType x); //Add x to graph
-    virtual void deleteEdge(ValueType source, ValueType dest);
-    virtual void deleteNode(ValueType node);
+    virtual bool adjacent(ValueType x, ValueType y) // is there a node from x to y
+    {
+
+    }
+    virtual vector<ValueType> neighbors(ValueType x) // Return a vector of neighbor nodes
+    {
+
+    }
+    virtual void addEdge(ValueType source, ValueType dest); //add an edge from source, to dest add dest if it isn't already in graph
+    {
+
+    }
+    virtual void addNode(ValueType x) //Add x to graph
+    {
+
+    }
+    virtual void deleteEdge(ValueType source, ValueType dest)
+    {
+
+    }
+    virtual void deleteNode(ValueType node)
+    {
+        
+    }
 };
--- include/adjacencymatrix.h
+++ include/adjacencymatrix.h
@@ -16,11 +16,28 @@ class AdjacencyMatrix : public Graph<ValueType>
 
     public: 
     // Assume that the class is templated by the type "ValueType"
-    virtual bool adjacent(ValueType x, ValueType y); // is there a node from x to y
-    virtual vector<ValueType> neighbors(ValueType x); // Return a vector of neighbor nodes
-    virtual void addEdge(ValueType source, ValueType dest); //add an edge from source, to dest
-                                                    // add dest if it isn't already in graph
-    virtual void addNode(ValueType x); //Add x to graph
-    virtual void deleteEdge(ValueType source, ValueType dest);
-    virtual void deleteNode(ValueType node);
+    virtual bool adjacent(ValueType x, ValueType y) // is there a node from x to y
+    {
+
+    }
+    virtual vector<ValueType> neighbors(ValueType x) // Return a vector of neighbor nodes
+    {
+
+    }
+    virtual void addEdge(ValueType source, ValueType dest) //add an edge from source, to dest add dest if it isn't already in graph
+    {
+
+    }
+    virtual void addNode(ValueType x) //Add x to graph
+    {
+
+    }
+    virtual void deleteEdge(ValueType source, ValueType dest)
+    {
+
+    }
+    virtual void deleteNode(ValueType node)
+    {
+        
+    }
 };
