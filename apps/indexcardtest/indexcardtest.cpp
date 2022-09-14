9f52f234ee491aaa81022f93b1c163b9f70442ba
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-09-13 22:53:36 -0500

    done..?

--- apps/indexcardtest/indexcardtest.cpp
+++ apps/indexcardtest/indexcardtest.cpp
@@ -11,10 +11,28 @@ using namespace std;
 
 int main ()
 {
-    IndexCards card;
+    IndexCard card;
+    const auto COUNT = 3;
+    int searchs[COUNT];
+    string words[COUNT] = {"gouda", "munster", "swiss"};
+    string defs[COUNT] = {"sweet, creamy, yellow cow's milk cheese originating from the Netherlands", 
+                    "soft washed rind cheese made from milk produced by cows in France", 
+                    "a yellow, medium-hard cheese that originated in Switzerland"};
 
+    //enter 3 cheese cards
+    for (auto i = 0; i < COUNT; i++)
+    {
+        searchs[i] = card.enterCard(words[i], defs[i]);
+    }
 
+    //search for the cards that do exist
+    for (auto j = 0; j < COUNT; j++)
+    {
+        card.searchCards(searchs[j]);
+    }
 
+    //search random ID that doesn't exist
+    card.searchCards(41687);
 
     return 0;
 }
