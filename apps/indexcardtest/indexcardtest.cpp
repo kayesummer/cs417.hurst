1802c91efdc5cd9575a393b299a380bb16541423
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-09-13 22:06:28 -0500

    indexcardtest setup

---
+++ apps/indexcardtest/indexcardtest.cpp
@@ -0,0 +1,20 @@
+//
+// File:    indexcardtest.cpp
+// Author: Kaylee Hurst  
+// Purpose: tests the indexcards.h class
+// 
+
+#include "indexcards.h"
+#include <iostream> 
+#include <string>
+using namespace std; 
+
+int main ()
+{
+    IndexCards card;
+
+
+
+
+    return 0;
+}
