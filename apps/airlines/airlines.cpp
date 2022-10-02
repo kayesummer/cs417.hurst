3df20fe2dae56b406e52ec86c480d51b137b2682
Kaylee Summer Hurst <1kayleesemail@gmail.com>
2022-10-02 16:04:07 -0500

    set up

---
+++ apps/airlines/airlines.cpp
@@ -0,0 +1,20 @@
+//
+// File:    airlines.cpp
+// Author: Kaylee Hurst  
+// Purpose: runs a simulation of airline queues 
+//
+
+#include <iostream> 
+#include <chrono> 
+#include <algorithm> 
+#include <utility> 
+#include <vector> 
+#include <string> 
+using namespace std; 
+
+int main ()
+{
+
+
+    return 0;
+}
